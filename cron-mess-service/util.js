072b9deea83ad7823a120f1e7c1caa6d314cdd72
Duc Tin <ductin.dev@gmail.com>
2023-05-30 20:30:32 +0700

    fefwf

--- cron-mess-service/util.js
+++ cron-mess-service/util.js
@@ -1,12 +1,12 @@
 var crypto = require("crypto");
 
-const logger = (channel, msg, time = `${new Date().toLocaleDateString()}-${new Date().toLocaleTimeString()}`) => {
+const logger = (channel, msg, time = `${new Date().toLocaleString()}`) => {
   console.log(`[${time}] Message {${msg}} sent to {${channel}}`);
 }
 
 const pharsedMsg = (text) => {
   return text.toString()
-    .replaceAll("${TIME}", new Date().toLocaleTimeString())
+    .replaceAll("${TIME}", new Date().toLocaleString())
     .replaceAll("${RANDOM}", crypto.randomBytes(20).toString('hex'))
     .replaceAll("${ID}", "N/A")
     .replaceAll("${NAME}", "System");
